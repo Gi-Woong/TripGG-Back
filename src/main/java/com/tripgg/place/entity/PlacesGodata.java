4f1e584f50c359f3ed6a6f48a8baa38bbf3eff44
dipito <rldndco@mju.ac.kr>
2025-09-14 15:31:32 +0900

    feat : 컬럼명 변경 및 공공데이터용 테이블 추가

---
+++ src/main/java/com/tripgg/place/entity/PlacesGodata.java
@@ -0,0 +1,51 @@
+package com.tripgg.place.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "places_godata")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class PlacesGodata {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "sigungu_name", nullable = false, length = 100)
+    private String sigunguName; // 시군명
+
+    @Column(name = "place_name", nullable = false, length = 255)
+    private String placeName; // 관광정보명
+
+    @Column(name = "phone_number", length = 50)
+    private String phoneNumber; // 전화번호
+
+    @Column(name = "data_reference_date")
+    private LocalDate dataReferenceDate; // 데이터기준일자
+
+    @Column(name = "road_address", length = 255)
+    private String roadAddress; // 정제도로명주소
+
+    @Column(name = "lot_address", length = 255)
+    private String lotAddress; // 정제지번주소
+
+    @Column(name = "postal_code", length = 10)
+    private String postalCode; // 정제우편번호
+
+    @Column(name = "latitude")
+    private Double latitude; // 정제WGS84위도
+
+    @Column(name = "longitude")
+    private Double longitude; // 정제WGS84경도
+}
