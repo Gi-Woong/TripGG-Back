2156414eb219bc735533211d56520baae495e03d
dipito <rldndco@mju.ac.kr>
2025-10-07 17:24:45 +0900

    fix: jackson LocalDateTime 오류 해결을 위한 수동 Bean 설정

--- src/main/java/com/tripgg/config/RestTemplateConfig.java
+++ src/main/java/com/tripgg/config/RestTemplateConfig.java
@@ -1,8 +1,12 @@
 package com.tripgg.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,9 +16,13 @@ public class RestTemplateConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-    
+
     @Bean
+    @Primary
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return Jackson2ObjectMapperBuilder.json()
+                .modules(new JavaTimeModule())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
     }
 }
